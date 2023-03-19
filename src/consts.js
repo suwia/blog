c942846cdc28bb6dd1b9176f64bc73a886e20973
suwian <suwian@foxmail.com>
2023-03-19 20:03:43 +0800

    fix: 合并源项目的代码优化

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
 export const SITE_TITLE = `Suwian's Blog`;
 export const SITE_DESCRIPTION = '做个神明，偏爱于你，漠视人间。';
 export const SITE_EMAIL = 'suwian@foxmail.com'
-export const SITE_NAME = 'astro.yufengbiji.com';
-export const SITE_URL = "https://astro.yufengbiji.com";
+export const SITE_NAME = 'suwian.zeabur.app';
+export const SITE_URL = "https://suwian.zeabur.app";
